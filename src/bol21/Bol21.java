93015522544a1d3f054d687a932f9eb49cf2a4cb
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-05 18:04:50 +0200

    FINAL

---
+++ src/bol21/Bol21.java
@@ -0,0 +1,19 @@
+
+package bol21;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class Bol21 {
+
+    
+    public static void main(String[] args) {
+        float base=2.0f,area ;        ///Se le pone f para que no cambia a double
+        float altura=(float)3.0; // Para practicar con el cast
+        area=base*altura/2;
+        
+        System.out.println("area="+area);
+    }
+    
+}
